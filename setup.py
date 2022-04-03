2ec6b37ab2e90f46bf0bc8587ae4c2309cfebaa3
Clayton Bezuidenhout <bezclayton@gmail.com>
2022-04-03 18:23:26 +0200

    feat(Initial): Add initial package (#1)
    * infra: add setup configuration
    
    - add LICENSE file
    - add semantic version config in setup.cfg
    
    * feat: add toml env loader
    
    - env.py file assembles at import and parses the environment variables into the toml-config dict.
    - entry get_active_toml_config can parse alternative files
    
    * infra: add package build script
    
    * feat: add inheritance between envs dev;test;acc;prod
    
    * refactor: move code into __init__ of package
    
    * test: add positive flow tests
    
    - infra: add pytest and coverage to setup.py
    - test: add test config files
    
    * style: apply black formatting
    
    * infra: add requirements.txt file
    
    Co-authored-by: Clayton Bezuidenhout <claytonbez.nl@gmail.com>

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+__version__ = "0.0.1"
+
+with open("./README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="active-toml-config",
+    version="0.0.1",
+    author="Clayton Bezuidenhout",
+    author_email="claytonbez.nl@gmail.com",
+    description="A small package to assist in loading and parsing toml config as env dict.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/claytonbez/active-toml-config",
+    project_urls={
+        "Bug Tracker": "https://github.com/claytonbez/active-toml-config/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(exclude=("tests", "dist", "sdist")),
+    install_requires=["pytoml", "pytest", "coverage", "flake8"],
+    python_requires=">=3.8",
+)
